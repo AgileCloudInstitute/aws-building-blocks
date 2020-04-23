8b3a5415837223686868fdaeaefa48c98647a46a
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-04-23 12:44:43 -0700

    Update variables.tf

--- modules/terraform-aws-simple-example-module/variables.tf
+++ modules/terraform-aws-simple-example-module/variables.tf
@@ -1,3 +1,6 @@
+## Copyright 2020 Green River IT (GreenRiverIT.com) as described in LICENSE.txt distributed with this project on GitHub.  
+## Start at https://github.com/AgileCloudInstitute?tab=repositories    
+
 # Using these data sources allows the configuration to be generic for any region.
 data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
