671196232f0b49bb929dbc38cdb857402cac211d
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-04-23 12:43:44 -0700

    Update providers.tf

--- modules/terraform-aws-simple-example-module/providers.tf
+++ modules/terraform-aws-simple-example-module/providers.tf
@@ -1,3 +1,5 @@
+## Copyright 2020 Green River IT (GreenRiverIT.com) as described in LICENSE.txt distributed with this project on GitHub.  
+## Start at https://github.com/AgileCloudInstitute?tab=repositories    
 
 provider "aws" {
   region     = "us-west-2"
