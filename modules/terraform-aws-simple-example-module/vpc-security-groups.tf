b318d3dbbd70c5d7c5c18b7ea9e2a92fac74046e
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-04-23 12:10:59 -0700

    Create vpc-security-groups.tf

---
+++ modules/terraform-aws-simple-example-module/vpc-security-groups.tf
@@ -0,0 +1,23 @@
+# Security group and rule to allow the node to communicate with the outside world.  
+
+resource "aws_security_group" "example-hosts" {
+  name        = "example node"
+  description = "Security group"
+  vpc_id      = aws_vpc.example-host.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group_rule" "example-admin-ssh" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks       = ["${local.admin-external-cidr}"]
+  security_group_id        = aws_security_group.example-hosts.id
+}
