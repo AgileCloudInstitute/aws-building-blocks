1f6bf46f62e455ac3d29a97a2f55919133f36dfe
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-04-23 12:47:37 -0700

    Update pipeline-network-apply.py

--- calls-to-modules/terraform-aws-simple-example-call-to-module/pipeline-network-apply.py
+++ calls-to-modules/terraform-aws-simple-example-call-to-module/pipeline-network-apply.py
@@ -1,3 +1,6 @@
+## Copyright 2020 Green River IT (GreenRiverIT.com) as described in LICENSE.txt distributed with this project on GitHub.  
+## Start at https://github.com/AgileCloudInstitute?tab=repositories    
+
 import subprocess
 
 path_to_input_vars="C:\\projects\\terraform-simple-aws-example\\vars\\VarsForTerraform\\"
