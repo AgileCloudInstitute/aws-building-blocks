e04ae5cfdbe649c96f15b6dea2e344626abaa952
JimGreenRiverIT <51681501+JimGreenRiverIT@users.noreply.github.com>
2020-05-01 15:42:20 -0700

    Update ec2-nodes.tf

--- modules/terraform-aws-simple-example-module/ec2-nodes.tf
+++ modules/terraform-aws-simple-example-module/ec2-nodes.tf
@@ -23,18 +23,18 @@ resource "aws_instance" "example-host" {
   ami                         = data.aws_ami.amazon-linux-2.id
   associate_public_ip_address = true
   instance_type               = "t2.micro"
-  #key_name                    = var.name_of_ssh_key
+  key_name                    = var.name_of_ssh_key
   user_data_base64 = base64encode(local.example-host-userdata)
   source_dest_check           = false
   subnet_id = aws_subnet.example-host.id
   vpc_security_group_ids = [aws_security_group.example-hosts.id]
 
-  #connection {
-  #  type        = "ssh"
-  #  user        = "ec2-user"
-  #  private_key = file("${var.path_to_ssh_keys}${var.name_of_ssh_key}.pem")
-  #  host = self.public_ip
-  #}
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("${var.path_to_ssh_keys}${var.name_of_ssh_key}.pem")
+    host = self.public_ip
+  }
 
   timeouts {
     create = "60m"
