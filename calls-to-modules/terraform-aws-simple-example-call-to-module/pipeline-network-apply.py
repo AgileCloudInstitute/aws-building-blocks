8d0e4cefb0a86493b4ab4eb3f0b86d75190acfa1
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-05-25 13:38:08 -0700

    Update pipeline-network-apply.py

--- calls-to-modules/terraform-aws-simple-example-call-to-module/pipeline-network-apply.py
+++ calls-to-modules/terraform-aws-simple-example-call-to-module/pipeline-network-apply.py
@@ -1,10 +1,15 @@
 ## Copyright 2020 Green River IT (GreenRiverIT.com) as described in LICENSE.txt distributed with this project on GitHub.  
 ## Start at https://github.com/AgileCloudInstitute?tab=repositories    
 
-import subprocess
+import subprocess  
+import sys  
+  
+region=sys.argv[1]  
+print("region variable is: ")  
+print(region)  
+  
+#path_to_input_vars="C:\\projects\\terraform-simple-aws-example\\vars\\VarsForTerraform\\"
 
-path_to_input_vars="C:\\projects\\terraform-simple-aws-example\\vars\\VarsForTerraform\\"
+#subprocess.run("terraform init", shell=True, check=True)
 
-subprocess.run("terraform init", shell=True, check=True)
-
-subprocess.run("terraform apply -auto-approve -var-file="+path_to_input_vars+"awspublickey.tfvars -var-file="+path_to_input_vars+"awsvpcmeta.tfvars -var-file="+path_to_input_vars+"awskeymeta.tfvars", shell=True, check=True)
+#subprocess.run("terraform apply -auto-approve -var-file="+path_to_input_vars+"awspublickey.tfvars -var-file="+path_to_input_vars+"awsvpcmeta.tfvars -var-file="+path_to_input_vars+"awskeymeta.tfvars", shell=True, check=True)
