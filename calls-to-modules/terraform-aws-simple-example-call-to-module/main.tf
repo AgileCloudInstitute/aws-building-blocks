fd3ea492e24fb1bfc0bfab5f8ce168654bddf7b1
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-05-25 16:25:51 -0700

    Update main.tf

--- calls-to-modules/terraform-aws-simple-example-call-to-module/main.tf
+++ calls-to-modules/terraform-aws-simple-example-call-to-module/main.tf
@@ -8,8 +8,8 @@ module "terraform-aws-simple-example" {
   _region = "us-west-2"
   access_key = "${var._public_access_key}"
   secret_access_key = "${var._secret_access_key}"
-  path_to_ssh_keys = "${var.path_to_ssh_keys}"
-  name_of_ssh_key = "${var.name_of_ssh_key}"
+  #path_to_ssh_keys = "${var.path_to_ssh_keys}"
+  #name_of_ssh_key = "${var.name_of_ssh_key}"
 
 }
 
@@ -17,8 +17,8 @@ module "terraform-aws-simple-example" {
 variable "aws_region" { }
 variable "_public_access_key" { }
 variable "_secret_access_key" { }
-variable "path_to_ssh_keys" { }
-variable "name_of_ssh_key" { }
+#variable "path_to_ssh_keys" { }
+#variable "name_of_ssh_key" { }
 
 ##Output variables
 output "public_ip_of_ec2_instance" { value = "${module.terraform-aws-simple-example.public_ip_of_ec2_instance}" }
