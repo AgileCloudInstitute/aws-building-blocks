aaf15697bc4f2b744f4b7690603c2bd581a01855
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-05-25 16:07:13 -0700

    Update pipeline-network-apply.py

--- calls-to-modules/terraform-aws-simple-example-call-to-module/pipeline-network-apply.py
+++ calls-to-modules/terraform-aws-simple-example-call-to-module/pipeline-network-apply.py
@@ -7,9 +7,24 @@ import sys
 region=sys.argv[1]  
 print("region variable is: ")  
 print(region)  
-  
+
+
+#'_public_access_key=$AWS_PUB'
+#'_secret_access_key=$AWS_SECRET'
+print("public key is: ")
+print($AWS_PUB)
+print("secret key is: ")
+print($AWS_SECRET)
+#'aws_region=region'
+
 #path_to_input_vars="C:\\projects\\terraform-simple-aws-example\\vars\\VarsForTerraform\\"
 
 #subprocess.run("terraform init", shell=True, check=True)
 
 #subprocess.run("terraform apply -auto-approve -var-file="+path_to_input_vars+"awspublickey.tfvars -var-file="+path_to_input_vars+"awsvpcmeta.tfvars -var-file="+path_to_input_vars+"awskeymeta.tfvars", shell=True, check=True)
+
+#subprocess.run("terraform apply -auto-approve 
+#               -var-file="+path_to_input_vars+"awspublickey.tfvars 
+#               -var-file="+path_to_input_vars+"awsvpcmeta.tfvars 
+#               -var-file="+path_to_input_vars+"awskeymeta.tfvars"
+#               , shell=True, check=True)
