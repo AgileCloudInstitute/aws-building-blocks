900fddc02fc667a4d7def182f07af18105af3c52
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-05-25 16:56:03 -0700

    Update pipeline-network-destroy.py

--- calls-to-modules/terraform-aws-simple-example-call-to-module/pipeline-network-destroy.py
+++ calls-to-modules/terraform-aws-simple-example-call-to-module/pipeline-network-destroy.py
@@ -1,8 +1,19 @@
 ## Copyright 2020 Green River IT (GreenRiverIT.com) as described in LICENSE.txt distributed with this project on GitHub.  
-## Start at https://github.com/AgileCloudInstitute?tab=repositories    
+## Start at https://github.com/AgileCloudInstitute?tab=repositories
 
-import subprocess
+import subprocess  
+import sys  
+import os  
+  
+region=sys.argv[1]  
+print("region variable is: ")  
+print(region)  
 
-path_to_input_vars="C:\\projects\\terraform-simple-aws-example\\vars\\VarsForTerraform\\"
+print("public key is: ")
+print(os.environ['AWS_PUB'])
+print("secret key is: ")
+print(os.environ['AWS_SECRET'])
 
-subprocess.run("terraform destroy -auto-approve -var-file="+path_to_input_vars+"awspublickey.tfvars -var-file="+path_to_input_vars+"awsvpcmeta.tfvars -var-file="+path_to_input_vars+"awskeymeta.tfvars", shell=True, check=True)
+destroyCommand="terraform destroy -auto-approve -var 'aws_region="+region+"' -var '_public_access_key="+os.environ['AWS_PUB']+"' -var '_secret_access_key="+os.environ['AWS_SECRET']+"'"
+
+subprocess.run(destroyCommand, shell=True, check=True)
