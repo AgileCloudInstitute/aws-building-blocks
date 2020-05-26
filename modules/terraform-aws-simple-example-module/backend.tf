c816613a83a30a4b5d1c101ad1d99796666ba939
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-05-26 11:53:37 -0700

    Update backend.tf

--- modules/terraform-aws-simple-example-module/backend.tf
+++ modules/terraform-aws-simple-example-module/backend.tf
@@ -1,6 +1,5 @@
 #IMPORTANT: Only use the below with a remote backend from pipeline.  
 #IMPORTANT: If you are running this locally as a simple example, make sure to comment out or delete the below to force a local backend instead.
 terraform {
-  backend "azurerm" {
-  }
+  backend "azurerm" { }
 }
