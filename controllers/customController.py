20999f20fb709c0871631e391d7f0ab72be4bd13
AgileCloudInstitute <Agile.Cloud.Institute@GreenRiverIT.com>
2022-08-08 14:51:23 -0700

    more upgrades

--- controllers/customController.py
+++ controllers/customController.py
@@ -17,13 +17,15 @@ print('templateFile is: ', templateFile)
 if not os.path.exists(templateFile):
   logString = "ERROR in customController. templateFile is not an actual file: "+str(templateFile)
   quit(logString)
+
 #Uncomment the next line to test throwing an error from the customController.
-#quit('ERROR in customController. ')
+#quit('Error in custom controller')
+
 print('inputArgs[0] is: ', inputArgs[0])
 print('inputArgs[1] is: ', inputArgs[1])
 print('inputArgs[2] is: ', inputArgs[2])
 
-#FOR TESTING ONLY: hard-coding output variables so you can see working example of format for output.
+#FOR TESTING ONLY: hard-coding output variables so you can see working example of format for output.  Your actual custom controller would need code to interpolate the values of output variables from whatever source system you are controlling.
 outputVars = [
 {'varName':'firstOutputVar', 'varValue':'value-for-first-output-variable'},
 {'varName':'secondOutputVar', 'varValue':'value-for-second-output-variable'}
@@ -34,6 +36,5 @@ if len(outputVars) > 0:
   for outputVariable in outputVars:
     print(outputVariable['varName'], ' = ', outputVariable['varValue'])
   print('Finished output variables.')
-print('---999---666---444')
-#quit('666!')
+print('About to exit custom controller.')
 exit(1)
